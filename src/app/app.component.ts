e5e72dcf48e8186e4a2574aea0c80bca0ca66f70
Sagir Hossain <sagir@impelitsolutions.com>
2025-11-18 00:20:43 +0600

    initial setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-practical-test';
+  constructor() {
+    setTheme('bs5');
+  }
 }
